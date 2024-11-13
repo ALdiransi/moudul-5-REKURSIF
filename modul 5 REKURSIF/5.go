3f0cd329717f1d50cb19a496ed88721b7191eaf1
aldi <162433306+ALdiransi@users.noreply.github.com>
2024-11-13 14:35:34 +0700

    Add files via upload

---
+++ modul 5 REKURSIF/5.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func printOddNumbers(n int) {
+	if n < 1 {
+		return
+	}
+	printOddNumbers(n - 2)
+	fmt.Print(n, " ")
+}
+
+func main() {
+	var num int
+	fmt.Print("Masukkan bilangan: ")
+	fmt.Scan(&num)
+
+	fmt.Println("Barisan bilangan ganjil:")
+	printOddNumbers(num)
+}
